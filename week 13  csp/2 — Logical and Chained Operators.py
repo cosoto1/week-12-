46301072a58981a7684482eb6a1705e42512019c
cosoto1 <Cosoto1@cps.edu>
2025-11-13 19:09:06 +0000

    Logical and Chained Operations

--- week 13  csp/2 — Logical and Chained Operators.py
+++ week 13  csp/2 — Logical and Chained Operators.py
@@ -20,12 +20,43 @@ print(1 < x < 20)         # True
 
 
 # Practice Problems:
+score = 89
+
+if score >= 90:
+    print("A")
+elif score >= 80:
+    print("B")
+elif score >= 70:
+    print("C")
+elif score >= 60:
+    print("D")
+else:
+    print("F")
+
 
 # Write an expression that checks if a number is between 50 and 100 (inclusive).
+number = 76
+50 <= number <= 100
 
 # Write an expression that checks if a number is NOT equal to 0 and greater than 10.
+number = 15
+
+if number != 0 and number > 10:
+    print("Number is not zero and greater than 10.")
+else:
+    print("Condition not met.")
 
 # Use chained comparison to check if 3 < 4 < 5.
+if 3 < 4 < 5:
+    print("True.")
+else:
+    print("False.")
 
 # Challenge: Create a password rule using logical operators:
+password = "my@securepass"
+
+if len(password) >= 8 and "@" in password and password != "password123":
+    print("Password is valid!")
+else:
+    print("Password does not meet the rules.")
 
